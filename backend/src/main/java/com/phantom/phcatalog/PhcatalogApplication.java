f065e6d9620d797582a85774c487fee413f53769
Henderson <hendalcosta@gmail.com>
2022-11-26 16:30:32 -0300

    Project Created

---
+++ backend/src/main/java/com/phantom/phcatalog/PhcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.phantom.phcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhcatalogApplication.class, args);
+	}
+
+}

3322663ac0e9eac466edac8e7499dfff7746bf42
Henderson <hendalcosta@gmail.com>
2022-11-27 20:36:21 -0300

    Category DTO

--- backend/src/main/java/com/phantom/phcatalog/services/CategoryService.java
+++ backend/src/main/java/com/phantom/phcatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.phantom.phcatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.phantom.phcatalog.dto.CategoryDTO;
 import com.phantom.phcatalog.entities.Category;
 import com.phantom.phcatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 }

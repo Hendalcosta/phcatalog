15c762066aab1c3461d0d1fec803bc6240fed352
Henderson <hendalcosta@gmail.com>
2022-12-20 11:01:53 -0300

    Auditing Data

--- backend/src/main/java/com/phantom/phcatalog/services/CategoryService.java
+++ backend/src/main/java/com/phantom/phcatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.phantom.phcatalog.dto.CategoryDTO;
 import com.phantom.phcatalog.entities.Category;
 import com.phantom.phcatalog.repositories.CategoryRepository;
+import com.phantom.phcatalog.services.exceptions.DatabaseException;
 import com.phantom.phcatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -54,6 +57,19 @@ public class CategoryService {
 		catch (EntityNotFoundException e ) {
 			throw new ResourceNotFoundException("ID não encontrado: " + id);
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("ID não encontrado: " + id);
+		}
+		catch (DataIntegrityViolationException e ) {
+			throw new DatabaseException("Violação de integridade de Banco de dados. Ação Negada");
+		}
+		
 	}
 	
 }

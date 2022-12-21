57c20f2a68756d6b7388cb49a204aa65541ac2ab
Henderson <hendalcosta@gmail.com>
2022-12-21 11:03:11 -0300

    Product, insert, update, delete

--- backend/src/main/java/com/phantom/phcatalog/services/ProductService.java
+++ backend/src/main/java/com/phantom/phcatalog/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.phantom.phcatalog.dto.CategoryDTO;
 import com.phantom.phcatalog.dto.ProductDTO;
+import com.phantom.phcatalog.entities.Category;
 import com.phantom.phcatalog.entities.Product;
+import com.phantom.phcatalog.repositories.CategoryRepository;
 import com.phantom.phcatalog.repositories.ProductRepository;
 import com.phantom.phcatalog.services.exceptions.DatabaseException;
 import com.phantom.phcatalog.services.exceptions.ResourceNotFoundException;
@@ -23,6 +26,9 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -40,7 +46,7 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		//entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 	}
@@ -49,7 +55,7 @@ public class ProductService {
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 		Product entity = repository.getOne(id); //em versões mais novas do JPA, o método getOne() foi substituído por getReferenceById()
-		//entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 		}
@@ -57,6 +63,7 @@ public class ProductService {
 			throw new ResourceNotFoundException("ID não encontrado: " + id);
 		}
 	}
+
 
 	public void delete(Long id) {
 		try {
@@ -67,6 +74,21 @@ public class ProductService {
 		}
 		catch (DataIntegrityViolationException e ) {
 			throw new DatabaseException("Violação de integridade de Banco de dados. Ação Negada");
+		}
+		
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
 		}
 		
 	}

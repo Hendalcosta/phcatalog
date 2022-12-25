9a1d693a3016f2fc07b517a1860f70aa5906208b
Henderson <hendalcosta@gmail.com>
2022-12-25 18:49:52 -0300

    ProductResourceTests Created; findAllShouldReturnPage test method, with mockMvc

---
+++ backend/src/test/java/com/phantom/phcatalog/resources/ProductResourceTests.java
@@ -0,0 +1,50 @@
+package com.phantom.phcatalog.resources;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.phantom.phcatalog.dto.ProductDTO;
+import com.phantom.phcatalog.services.ProductService;
+import com.phantom.phcatalog.tests.Factory;
+
+@WebMvcTest(ProductResource.class)
+public class ProductResourceTests {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private ProductService service;
+	
+	private ProductDTO productDTO;
+	private PageImpl<ProductDTO> page;
+	
+	@BeforeEach
+	void setUp() {
+		
+		productDTO = Factory.createProductDTO();
+		page = new PageImpl<>(List.of(productDTO));
+		
+		when(service.findAllPaged(any())).thenReturn(page);
+		
+	}
+	
+	@Test
+	public void findAllShouldReturnpage() throws Exception{
+		mockMvc.perform(get("/products")).andExpect(status().isOk());
+	}
+	
+
+}

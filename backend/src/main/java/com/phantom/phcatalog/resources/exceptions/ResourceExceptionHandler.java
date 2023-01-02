8dcd8bdb3cacbb0f850e4e2fc383d8f025e4159f
Henderson <hendalcosta@gmail.com>
2023-01-02 10:57:04 -0300

    Custom validation response

--- backend/src/main/java/com/phantom/phcatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/phantom/phcatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -39,4 +41,21 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	}
 	
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		for(FieldError f : e.getBindingResult().getFieldErrors()) { //getBindingResult() captura os erros especificos da validação; getFieldErrors() gera uma lista na resposta da requisição; 
+			err.addError(f.getField(), f.getDefaultMessage());//getField() obtem os campos onde ocorreu erros de validação; getDefaultMessage() obtem a menssagem padrão passada nas annotations nas classes DTO. 
+		}
+		
+		return ResponseEntity.status(status).body(err);
+	}
 }

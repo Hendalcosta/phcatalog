9b982d44c96e2db4db37e1e62f1935579acd46fd
Henderson <hendalcosta@gmail.com>
2023-01-02 10:22:11 -0300

    Basic Beans validation

--- backend/src/main/java/com/phantom/phcatalog/dto/UserDTO.java
+++ backend/src/main/java/com/phantom/phcatalog/dto/UserDTO.java
@@ -4,14 +4,21 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.phantom.phcatalog.entities.User;
 
 public class UserDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotBlank(message = "Campo obrigatório")
 	private String firstName;
 	private String lastName;
+	
+	@Email(message = "Entre um e-mail válido")
 	private String email;
 	
 	

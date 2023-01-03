203debe3b2f8c25ab900dc8127e20645b32a0954
Henderson <hendalcosta@gmail.com>
2023-01-03 19:32:08 -0300

    Evironments Variables

--- backend/src/main/java/com/phantom/phcatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/phantom/phcatalog/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.phantom.phcatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,18 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAdapter{
+	
+	@Value("${security.oauth2.client.client-id}")
+	private String clientId;
+	
+	@Value("${security.oauth2.client.client-secret}")
+	private String clientSecret;
+	
+	@Value("${jwt.duration}")
+	private Integer jwtDuration;
+	
+	
+	
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -37,11 +50,11 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
-		.withClient("phcatalog")
-		.secret(passwordEncoder.encode("phcatalog123"))
+		.withClient(clientId)
+		.secret(passwordEncoder.encode(clientSecret))
 		.scopes("read", "write")
 		.authorizedGrantTypes("password")
-		.accessTokenValiditySeconds(86400);
+		.accessTokenValiditySeconds(jwtDuration);
 	}
 
 	@Override

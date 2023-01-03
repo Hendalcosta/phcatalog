d1ccac4fbac298685f1031962a82ad2f706bddc8
Henderson <hendalcosta@gmail.com>
2023-01-03 19:49:19 -0300

    Token additional information

--- backend/src/main/java/com/phantom/phcatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/phantom/phcatalog/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.phantom.phcatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +12,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import com.phantom.phcatalog.components.JwtTokenEnhancer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -26,9 +31,6 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 	@Value("${jwt.duration}")
 	private Integer jwtDuration;
 	
-	
-	
-	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 	
@@ -41,6 +43,8 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private JwtTokenEnhancer tokenEnhancer;
 	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -59,9 +63,13 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		TokenEnhancerChain chain = new TokenEnhancerChain();
+		chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, tokenEnhancer));
+		
 		endpoints.authenticationManager(authenticationManager)
 		.tokenStore(tokenStore)
-		.accessTokenConverter(accessTokenConverter);
+		.accessTokenConverter(accessTokenConverter)
+		.tokenEnhancer(chain);
 	}
 	 
 	
